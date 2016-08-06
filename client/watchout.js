00a5a188c59c27348f8f5252b6c6524a5824854e
cihannisanci <cihannisan@gmail.com>
2016-08-06 11:09:59 -0700

    Collisions Recorded Well

--- client/watchout.js
+++ client/watchout.js
@@ -1,5 +1,5 @@
 // start slingin' some d3 here.
-var update = function() {  
+var updatePosition = function() {  
   var randomArray = [];
 
   for (var i = 0; i < 25; i++) {
@@ -11,15 +11,30 @@ var update = function() {
   var dataArray = randomArray;
 
   svg.selectAll('circle').data(dataArray).enter().append('circle').classed('enemy', true);
-  d3.selectAll('.enemy').transition().duration(1200).attr('cx', function(d) { return d[0]; }).attr('cy', function(d) { return d[1]; }).attr('r', '10');
 
-  console.log('running');
+  var enemies = d3.selectAll('.enemy');
+  
+  enemies.transition().duration(1800).attr('cx', function(d) { return d[0]; }).attr('cy', function(d) { return d[1]; });
 
 };
 
+var updateCollisions = function() {
+
+  enemies.each(function() {
+    var enemy = d3.select(this);
+    var distance = Math.sqrt(Math.pow((enemy.attr('cx') - player.attr('cx')), 2) + Math.pow((enemy.attr('cy') - player.attr('cy')), 2));
+    if (distance < 20) {
+      collisionCount++;
+    }
+  });
+
+  console.log(collisionCount);
+};
+
+
 var svg = d3.select('body').append('svg').attr('height', '450').attr('width', '700');
 
-var player = svg.append('circle').classed('player', true).attr('cx', '350').attr('cy', '225').attr('r', '10');
+var player = svg.append('circle').classed('player', true).attr('cx', '350').attr('cy', '225');
 
 var drag = d3.drag().on('drag', function() { 
   player.attr('cx', d3.event.x).attr('cy', d3.event.y); 
@@ -27,12 +42,15 @@ var drag = d3.drag().on('drag', function() {
 
 player.call(drag);
 
+var collisionCount = 0;
 
-update();
+updatePosition();
 
+var enemies = d3.selectAll('.enemy');
 
+setInterval(updatePosition, 2000);
 
-setInterval(update, 1300);
+setInterval(updateCollisions, 50);
 
 // update();
 
